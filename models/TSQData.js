fbbd3b3ccd59284283bdf68b9166ca88835ac0c8
Tina Orozco <tmorozco1990@gmail.com>
2019-02-07 13:10:48 -0700

    build routes tsqData root and createTSQ
    ###Changes:
    
    **/models/TSQData:**
    - languageList field is now skillList
    - add comma on line 10
    - add comma on line 13
    - lines 21-25, add createTSQ function
    
    **/routes/tsq/tsqData.js**
    - line 3, require db config
    - line 5, require TSQData model
    - build get route for / 
    - build post route for /createTSQ

--- models/TSQData.js
+++ models/TSQData.js
@@ -3,14 +3,14 @@ const config = require('../config/database')
 const rand = require('random-key')
 
 const TSQDataSchema = mongoose.Schema({
-	languageList: {
+	skillList: {
 		type: Array,
 		default: () => [],
 		required: true
-	}
+	},
 	uKey: {
 		type: String,
-		default: () => rand.generate()
+		default: () => rand.generate(),
 		required: true
 	}
 })
@@ -18,6 +18,11 @@ const TSQDataSchema = mongoose.Schema({
 const TSQData = module.exports = mongoose.model('TSQ_Data', TSQDataSchema)
 
 // db functions
+module.exports.createTSQ = function(data, callback) {
+	data.ukey = rand.generate()
+ 	data.save(callback)
+}
+
 module.exports.getTSQDataById = function(id, callback) {
 	TSQData.findById(id, callback)
 }
--- routes/tsq/tsqData.js
+++ routes/tsq/tsqData.js
@@ -1,4 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const config = require('../../config/database')
+
+const TSQData = require('../../models/TSQData')
+
+router.get('/', (req, res, next) => {
+	// TODO: send a documentation page on this endpoint here!
+	res.send('This endpoint is for Technical Skills Questionaire data!')
+})
+
+router.post('/createTSQ', (req, res, next) => {
+	let newTSQ = new TSQData({
+		skillList: req.body.skillList,
+	})
+
+	TSQData.createTSQ(newTSQ, (err, tsqdata) => {
+		if (err) {
+			res.json({ success: false, msg: 'Failed to create TSQ Data Entry'})
+		} else {
+			res.json({ success: true, msg: 'TSQ Data Entry Created!'})
+		}
+	})
+})
 
 module.exports = router
