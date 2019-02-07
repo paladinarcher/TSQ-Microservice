53595b86025d7b54686e608f792d30ec67cfc2da
Tina Orozco <tmorozco1990@gmail.com>
2019-02-07 10:54:49 -0700

    create and register routes
    ###Changes:
    **new files:** 
    - add /routes/tsq/frameworks.js
    - add /routes/tsq/libraries.js
    - add /routes/tsq/markupLanguages.js
    - add /routes/tsq/programmingLanguages.js
    - add /routes/tsq/markupLanguages.js
    - add /routes/tsq/queryLanguages.js
    - add /routes/tsq/skillSearch.js
    - add /routes/tsq/stylesheetLanguages.js
    - add /routes/tsq/concepts.js
    
    **/app.js:**
    lines 20-28, require new route files
    lines 41-49, register routes

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ mongoose.connection.on('error', (err) => console.log('DATABASE ERROR: ' + err))
 const app = express()
 
 // routes
-const tsq = require('./routes/tsq')
+const tsqData = require('./routes/tsq/tsqData')
+const skillSearch = require('./routes/tsq/skillSearch')
+const programmingLanguages = require('./routes/tsq/programmingLanguages')
+const markupLanguages = require('./routes/tsq/markupLanguages')
+const queryLanguages = require('./routes/tsq/queryLanguages')
+const stylesheetLanguages = require('./routes/tsq/stylesheetLanguages')
+const libraries = require('./routes/tsq/libraries')
+const frameworks = require('./routes/tsq/frameworks')
+const concepts = require('./routes/tsq/concepts')
 
 // port variable
 const port = 4000
@@ -30,7 +38,15 @@ app.use(bodyParser.json())
 // app.use(passport.session())
 // require('./config/passport')(passport)
 
-app.use('/tsq', tsq)
+app.use('/tsq/tsqData', tsqData)
+app.use('/tsq/skillSearch', skillSearch)
+app.use('/tsq/markupLanguages', markupLanguages)
+app.use('/tsq/programmingLanguages', programmingLanguages)
+app.use('/tsq/stylesheetLanguages', stylesheetLanguages)
+app.use('/tsq/queryLanguages', queryLanguages)
+app.use('/tsq/libraries', libraries)
+app.use('/tsq/frameworks', frameworks)
+app.use('/tsq/concepts', concepts)
 
 // route settings
 app.get('/', (req, res) => res.send('Hello from TSQ!'))
