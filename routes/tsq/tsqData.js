82d9fc332f63c6dbec9275eb25613690896b30d3
Tina Orozco <tmorozco1990@gmail.com>
2019-02-07 14:23:27 -0700

    set up query by id param for tsqData endpoint
    ### Changes: 
    
    **/routes/tsq/tsqData.js**
    - lines 4-5, import url and querystring
    - lines 10-35, refactor get '/' route to include a query parameter of id

--- routes/tsq/tsqData.js
+++ routes/tsq/tsqData.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const config = require('../../config/database')
-
+const url = require('url');
+const querystring = require('querystring');
 const TSQData = require('../../models/TSQData')
 
 router.get('/', (req, res, next) => {
 	// TODO: send a documentation page on this endpoint here!
-	res.send('This endpoint is for Technical Skills Questionaire data!')
+	console.log(req.params)
+
+	if (req.query.id) {
+		let id = req.query.id
+		TSQData.getTSQDataById(id, (err, data) => {
+			if (err) {
+				return res.json({
+					success: false,
+					msg: 'There was an error ',
+					error: err
+				})
+			}
+			if (!data) {
+				res.status(404)
+				return res.json({
+					success: false,
+					msg: 'technical skills questionaire entry not found'
+				})
+			}
+			return res.json({ success: true, TSQData: data })
+		})
+
+	} else {
+		res.send('This endpoint is for Technical Skills Questionaire data!')
+	}
+
 })
 
 router.post('/createTSQ', (req, res, next) => {
