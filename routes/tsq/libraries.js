8d8dd5bb6d3b50ee2f5394cca755fd70fe3dd948
Tina Orozco <tmorozco1990@gmail.com>
2019-02-13 14:48:21 -0700

    add routes to /routes/tsq/libraries.js
    Changes: 
    - added boilerplate for library routes and imported LibraryData

--- routes/tsq/libraries.js
+++ routes/tsq/libraries.js
@@ -1,4 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const LibraryData = require('../../models/Libraries')
+
+
+// POST
+router.post('/libraries/addLibraryData', (req, res, next) => {})
+
+// GET
+router.get('/libraries/', (req, res, next) => {})
+
+// PUT
+router.put('/libraries/updateLibraryData/:id', (req, res, next) => {})
+
+// DELETE
+router.delete('/libraries/removeLibraryData/:id', (req, res, next) => {})
 
 module.exports = router
