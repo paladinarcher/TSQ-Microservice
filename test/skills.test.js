0f04e12f0e4cc54dceba358ada9183577d420138
Tina Orozco <tmorozco1990@gmail.com>
2019-02-26 13:48:19 -0700

    finish writing tests for /skills endpoint

--- test/skills.test.js
+++ test/skills.test.js
@@ -111,16 +111,81 @@ describe('Skills API Tests', () => {
           done()
         })
     })
-  	it('it queries all the skills entries by tags')
+  	it('it queries all the skills entries by tags', done => {
+      addSkillEntry(TestData.testSkill2)
+      const TAG = 'tag-1'
+
+      chai.request(server)
+        .get(URL + '/?tags=' + TAG)
+        .end((error, response) => {
+          chai.assert(response.body,
+            'response body does not exist')
+          chai.assert(response.status === 200,
+            'response status is not 200')
+          chai.assert(response.body.success === true,
+            'success is not true')
+          done()
+        })
+    })
   })
 
   describe('/UPDATE /skills/', () => {
-  	it('it updates skill name')
-  	it('it updates skill tags')
+  	it('it updates skill name', done => {
+      addSkillEntry(TestData.testSkill1)
+      const updatedName = 'updated-name'
+      const dataToSend = { name: updatedName }
+      chai.request(server)
+        .put(URL + '/updateName/' + TestData.testSkill1._id)
+        .send(dataToSend)
+        .end((error, response) => {
+          chai.assert(response.body,
+            'response body does not exist')
+          chai.assert(response.status === 200,
+            'response status is not 200')
+          chai.assert(response.body.success === true,
+            'success is not true')
+          chai.assert(response.body.data.nModified === 1,
+            '1 entry was modified')
+          done()
+        })
+    })
+  	it('it updates skill tags', done => {
+      addSkillEntry(TestData.testSkill1)
+      const updatedTags = ['tag1', 'tag2']
+      const dataToSend = { tags: updatedTags }
+      chai.request(server)
+        .put(URL + '/updateTags/' + TestData.testSkill1._id + '?append=true')
+        .send(dataToSend)
+        .end((error, response) => {
+          chai.assert(response.body,
+            'response body does not exist')
+          chai.assert(response.status === 200,
+            'response status is not 200')
+          chai.assert(response.body.success === true,
+            'success is not true')
+          chai.assert(response.body.data.nModified === 1,
+            '1 entry was modified')
+          done()
+        })
+    })
   })
 
 
   describe('/DELETE /skills/', () => {
-  	it('it removes skill entries by id')
+  	it('it removes skill entries by id', done => {
+      addSkillEntry(TestData.testSkill1)
+      const ID = TestData.testSkill1._id
+      chai.request(server)
+        .del(URL + '/removeEntry/' + ID)
+        .end((error, response) => {
+          chai.assert(response.body,
+            'response body does not exist')
+          chai.assert(response.status === 200,
+            'response status is not 200')
+          chai.assert(response.body.success === true,
+            'success is not true')
+          done()
+        })
+    })
   })
 })
