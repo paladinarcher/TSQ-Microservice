2ffec1baf7f0b4372c1e842293ba66eaeb1a4661
Wayne Jensen <jensenw@gmail.com>
2019-08-08 16:46:45 -0600

    prod server change

--- pipeline/prod/docker-build.sh
+++ pipeline/prod/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/tsq/pipeline/prod/
+cd /home/ec2-user/src/tsq/pipeline/prod/
 sudo /usr/local/bin/docker-compose down
 sudo /usr/local/bin/docker-compose build
 sudo /usr/local/bin/docker-compose up -d
